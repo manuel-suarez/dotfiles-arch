1c8cf5a5b6c80f693336781816b8ced1b7ce4e67
Arturo Suárez <manuel.suarez@cimat.mx>
2024-04-08 07:51:25 -0600

    Added Zsh configuration

--- install.sh
+++ install.sh
@@ -13,3 +13,4 @@
 mkdir -p "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
